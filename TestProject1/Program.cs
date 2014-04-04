447fabbaaab4077a3bb3bc947960cca95ac04155
dudelis <fukszon.kostya@gmail.com>
2014-04-04 06:39:59 -0700

    TestProject

---
+++ TestProject1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace TestProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.WorkListItemActionint("2040_87;2041_53;", "Sign");          
+               
+            
+
+        }
+        
+
+               
+    }
+}
