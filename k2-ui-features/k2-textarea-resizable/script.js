b74b42472f8fd5664ab8d9084b6139c3c3875fe4
Konstantin Fukszon <fukszon.kostya@gmail.com>
2019-03-04 06:18:12 -0800

    Add textarea resize trick

--- k2-ui-features/k2-textarea-resizable/script.js
+++ k2-ui-features/k2-textarea-resizable/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-	$('textarea').resizable();
+    $('textarea').each(function(index, obj){
+        $wrapper = $(obj).closest('.input-control.text-input');
+        var width = $wrapper.width();
+        $wrapper.css('width', '');
+        $(obj).css('width', width + 'px');
+        $(obj).resizable();
+    });
 });
