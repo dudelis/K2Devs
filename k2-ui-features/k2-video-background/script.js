33e74fa42cbc1ea1217433f22db0cf309ac5b024
Konstantin Fukszon <fukszon.kostya@gmail.com>
2019-04-01 14:10:33 -0700

    Add webm video

--- k2-ui-features/k2-video-background/script.js
+++ k2-ui-features/k2-video-background/script.js
@@ -1,3 +1,4 @@
-const html = '<div id="k2-background-video" class="bg-video"><video class="bg-video__content" autoplay="" muted="" loop="">Your browser is not supported</video></div>';
+const html =
+    '<div id="k2-background-video" class="bg-video"><video class="bg-video__content" autoplay muted loop><source src="https://github.com/dudelis/K2Devs/raw/master/k2-ui-features/k2-video-background/video.mp4" type="video/mp4">Your browser is not supported</video></div>';
 
 $('.runtime-content').prepend(html);
