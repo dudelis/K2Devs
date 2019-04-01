1aeb5e165f2620f8e59d9a0d85a8114bbf1057c8
Konstantin Fukszon <fukszon.kostya@gmail.com>
2019-04-01 13:57:25 -0700

    Add video for testing

---
+++ k2-ui-features/k2-video-background/script.js
@@ -0,0 +1,3 @@
+const html = '<div id="k2-background-video" class="bg-video"><video class="bg-video__content" autoplay="" muted="" loop="">Your browser is not supported</video></div>';
+
+$('.runtime-content').prepend(html);
